af2e38ed8b4f5f49e668c16a189192dc56aa39e6
GW <kowonp@gmail.com>
2021-08-24 19:31:58 +0900

    for, range, if else, Pointer

--- aboutgo/main.go
+++ aboutgo/main.go
@@ -13,6 +13,59 @@ import (
 	// "github.com/githubgw/learngo/something"
 )
 
+func canIDrink(age int) bool{
+	// go에서의 if문 
+	// go에서는 if문에 조건을 ()괄호로 묶어줄 필요없이 바로 쓰면 된다.
+	// go의 if문에서는 if문 안에서 변수를 생성하고 바로 가져와 쓸 수 있다.  
+	// if koreanAge := age+2; koreanAge <18{
+	// 	return false;
+	// }else {
+	// 	return true;
+	// }
+
+	// go에서의 switch문 
+	// switch {
+	// 	case age < 18:
+	// 		return false;
+	// 	case age >=18:
+	// 		return true;
+	// 	default:
+	// 		return false;
+	// }
+
+	switch koreanAge:=age+2; koreanAge{
+		case 10:
+			return false;
+		case 18:
+			return true;
+		default:
+			return false;
+	}
+
+}
+
+func superAdd(numbers ...int) int {
+	// fmt.Println("numbers",numbers)
+
+	total:=0;
+
+	// for와 range를 이용해서 자바스크립트의 for처럼 반복문을 만들 수 있다.
+	// range는 기본적으로 index를 준다.(순번을 확인할 수 있음)
+	// for index,number := range numbers{ }
+	// index를 안 쓸 때는 _로 비워두면 된다.
+	for _,number := range numbers{
+		total=total+number;
+	}
+
+	// 위의 코드를 for문만을 이용해서도 만들 수 있다. 
+	// for i:=0; i<len(numbers); i++{
+	// 	fmt.Println(numbers[i])
+	// }
+
+	return total;
+}
+
+
 // 만약 함수의 파라미터로 여러 값을 받고 싶다면 아래와 같이 ...을 붙여주면 된다.
 func repeatMe(words ...string){
 	fmt.Println(words)
@@ -64,6 +117,7 @@ func main() {
 	// var name2 string = "cody"
 	// name2 = "sugar"
 
+	// 변수명 := 값
 	// 위의 코드를 아래와 같이 축약해서 작성할 수도 있다. 단, 축약형은 func인 함수내부에서만 사용가능하다. 
 	// 함수 밖에서는 원래 변수를 만드는 형태로 만들 수 있다.
 	// 만약 아래와 같이 작성하면 Go가 알아서 값에 대한 타입을 찾아준다.
@@ -83,8 +137,34 @@ func main() {
 	totalLength, upperName := lengthAndUppercase("GitHubGW")
 	fmt.Println(totalLength, upperName)
 
-	repeatMe("Tom", "Cody", "Conor", "Dana")
+	// repeatMe("Tom", "Cody", "Conor", "Dana")
 
 	// fmt.Println(lengthAndUppercase2("Colby"))
-	lengthAndUppercase2("Colby")
+	// lengthAndUppercase2("Colby")
+
+	result:=superAdd(1,2,3,4,5,6)
+	fmt.Println(result)
+
+	fmt.Println(canIDrink(16))
+
+	// a:=2;
+	// b:=a;
+	// a=10;
+	// a와 b앞에 &a, &b를 붙이게 되면 해당 변수의 메모리 주소를 볼 수 있다.
+	// fmt.Println(&a,&b)
+
+	// 아래와 같이 a의 메모리 주소를 b에 넣어주게 되면 a와 b는 같은 메모리 주소를 가지게 된다.
+	// a:=2;
+	// b:=&a;
+	// fmt.Println(&a,b)
+
+	// 그리고 *는 메모리 주소를 통해 해당 주소 안에 담긴 값을 살펴볼 수 있다.
+	// b:=&a를 통해 a의 메모리 주소를 b에 넣었고, a와 b를 연결했기 떄문에 a의 값이 바뀌면 b의 값도 바뀌게 된다.
+	// 즉 b는 a의 메모리를 살펴보고 그 안의 값을 가진다. (b는 a의 포인터가 된다.)
+	// *b는 메모리 주소 안의 값에 접근해서 값을 변경할 수도 있다. 
+	a:=2;
+	b:=&a;
+	a=5;
+	*b=20;
+	fmt.Println(a,b)
 }
